503d3c989bbec7876f94f04e6055556e055602fc
KangJuSung <appie701@naver.com>
2022-02-16 23:50:29 +0900

    feat : models.py

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    email = models.CharField(max_length=50, primary_key=True)
+    password = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.email
+
+
+class Color(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    color = models.CharField(max_length=6)
+    date = models.DateTimeField()
+
+    def __str__(self):
+        return self.color
+
+
+class Fashion(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    color = models.CharField(max_length=6)
+    img_url = models.URLField()
+    rate = models.FloatField()
+    date = models.DateTimeField()
+    
+
+
+

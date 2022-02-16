b7cd76168f4e9b24180cb092f2b38a98ffd1c835
baegyeongtae <bkt584@gmail.com>
2022-02-17 02:42:44 +0900

    사진올리는 컴포넌트

---
+++ frontend/src/components/PhotoUpload.js
@@ -0,0 +1,19 @@
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import AddPhotoAlternateIcon from '@mui/icons-material/AddPhotoAlternate';
+
+export default function PhotoUpload() {
+    return (
+        <Box
+            component="span"
+            sx={{
+                p: 10,
+                border: '2px dashed grey',
+            }}
+        >
+            <Button>
+                <AddPhotoAlternateIcon />
+            </Button>
+        </Box>
+    );
+}

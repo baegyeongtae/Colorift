21efdeb1b65829c8a822a711c3e263d25fe9c7ff
baegyeongtae <bkt584@gmail.com>
2022-02-18 02:11:48 +0900

    분석 결과 div demo

--- frontend/src/components/Result.js
+++ frontend/src/components/Result.js
@@ -8,14 +8,10 @@ export default function Result() {
 const BoxDiv = styled.div`
     @media ${({ theme }) => theme.device.mobile} {
         background-color: ${({ theme }) => theme.color.lightgray};
-        position: absolute;
+        position: relative;
         width: 222.94px;
         height: 161.07px;
-        left: 86.36px;
-        top: 206px;
-
         background: #c4c4c4;
-        transform: matrix(1, 0, 0, 1, 0, 0);
     }
 
     color: ${({ theme }) => theme.color.blue};

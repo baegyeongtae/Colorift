6086e248c91cfba143d56c690a05aaf3cd317121
KangJuSung <appie701@naver.com>
2022-02-18 23:07:54 +0900

    feat : model 수정, gitignore추가

--- backend/app/models.py
+++ backend/app/models.py
@@ -5,32 +5,17 @@ class User(models.Model):
     email = models.CharField(max_length=50, primary_key=True)
     password = models.CharField()
 
-    def __str__(self):
-        return self.email
-
 
 class Color(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     color = models.CharField(max_length=6)
-    img_url = models.URLField()
+    image = models.ImageField()
     date = models.DateField()
-
-    def __str__(self):
-        return self.color
 
 
 class Fashion(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     color = models.CharField(max_length=6)
-    img_url = models.URLField()
+    image = models.ImageField()
     rate = models.FloatField()
     date = models.DateField()
-
-
-class ColorInput(models.Model):
-    photo = models.ImageField()
-
-
-class FashionInput(models.Model):
-    color = models.CharField(max_length=6)
-    photo = models.ImageField()
--- backend/config/settings.py
+++ backend/config/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'django.apps.ColofitConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',

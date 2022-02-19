64995be66fd70bd8cf6366129a06308497cc2b1d
baegyeongtae <bkt584@gmail.com>
2022-02-19 14:06:30 +0900

    container div component추가로 수정 중

--- frontend/src/pages/personalcolor/Loading.js
+++ frontend/src/pages/personalcolor/Loading.js
@@ -1,36 +1,34 @@
 import styled from 'styled-components';
-import Footer from '../../components/Footer';
+import { Footer, ContainerDiv } from '../../components';
 import loader from '../../images/loader.png';
 
 function Loading() {
     return (
         <>
-            <ContainerCircleDiv>
-                <LeftCircleDiv>
-                    <TextH2>
-                        얼굴 사진 <br /> 업로드
-                    </TextH2>
-                </LeftCircleDiv>
-
-                <CurrentCircleDiv>
-                    <LineDiv>
+            <CircleContainerDiv>
+                <div className="wrapper">
+                    <div className="current_circle">
+                        <TextH2>
+                            얼굴 사진 <br /> 업로드
+                        </TextH2>
+                    </div>
+                    <div className="left_circle">
                         <TextH2>
                             피부톤 <br />
                             정밀 분석
                         </TextH2>
-                    </LineDiv>
-                </CurrentCircleDiv>
-
-                <LeftCircleDiv>
-                    <TextH2>
-                        퍼스널 컬러 <br /> 분석 결과
-                    </TextH2>
-                </LeftCircleDiv>
-            </ContainerCircleDiv>
-            <ContainerDiv>
+                    </div>
+                    <div className="left_circle">
+                        <TextH2>
+                            퍼스널 컬러 <br /> 분석 결과
+                        </TextH2>
+                    </div>
+                </div>
+            </CircleContainerDiv>
+            <LoadingContainerDiv>
                 <img src={loader} alt="loader" width="130px" height="130px" />
                 <TextH1>피부톤을 분석하는 중입니다.</TextH1>
-            </ContainerDiv>
+            </LoadingContainerDiv>
 
             <Footer />
         </>
@@ -41,64 +39,115 @@ export { Loading };
 
 // styled-components
 
-const CurrentCircleDiv = styled.div`
+const CircleContainerDiv = styled(ContainerDiv)`
     @media ${({ theme }) => theme.device.mobile} {
-        background-color: ${({ theme }) => theme.color.blue};
+        all: unset;
 
-        width: 73.36px;
-        height: 73.36px;
-        border-radius: 75px;
-        text-align: center;
-        margin-top: 86px;
-        margin-left: 45px;
-        margin-bottom: 10px;
-        font-size: 12px;
-        color: #fff;
-    }
-
-    color: ${({ theme }) => theme.color.blue};
-    background-color: ${props => props.theme.color.blue};
-`;
-
-const LeftCircleDiv = styled.div`
-    @media ${({ theme }) => theme.device.mobile} {
-        background-color: ${({ theme }) => theme.color.lightgray};
-        width: 73.36px;
-        height: 73.36px;
-        border-radius: 75px;
-        text-align: center;
-        margin-top: 86px;
-        margin-left: 45px;
-        margin-bottom: 10px;
-        font-size: 12px;
-        color: #fff;
-    }
-    color: ${({ theme }) => theme.color.white};
-    background-color: ${props => props.theme.color.blue};
-`;
-
-const ContainerCircleDiv = styled.div`
-    @media ${({ theme }) => theme.device.mobile} {
-        background-color: ${({ theme }) => theme.color.white};
-        display: flex;
-        margin-bottom: 20px;
-    }
-    color: ${({ theme }) => theme.color.white};
-    background-color: ${props => props.theme.color.white};
-`;
-
-const ContainerDiv = styled.div`
-    @media ${({ theme }) => theme.device.mobile} {
         background-color: ${({ theme }) => theme.color.white};
         display: flex;
         flex-direction: column;
         justify-content: space-around;
         align-items: center;
-        margin-bottom: 30px;
-        margin-top: 120px;
+        margin-bottom: 20px;
+        margin-top: 70px;
+        color: white;
+        
+        .wrapper {
+            width: 100%;
+            height: auto;
+            position: relative;
+            display: flex;
+            justify-content: space-evenly;
+            align-items: center;
+            margin-bottom: 7px;
+            padding-left: 30px;
+            padding-right: 30px;
+        }
+        .wrapper::before {
+            content: '';
+            width: 50%;
+            height: 2px;
+            background-color: ${({ theme }) => theme.color.lightgray};
+            position: absolute;
+            top: 50%;
+            left: 100;
+        }
+        .current_circle {
+            width: 68px;
+            height: 68px;
+            position: relative;
+            border-radius: 50%;
+            background-color: ${({ theme }) => theme.color.blue};
+            text-align: center;
+        }
+        .left_circle {
+            width: 68px;
+            height: 68px;
+            position: relative;
+            border-radius: 50%;
+            background-color: ${({ theme }) => theme.color.lightgray};
+            text-align: center;
+        }
     }
-    color: ${({ theme }) => theme.color.white};
-    background-color: ${props => props.theme.color.white};
+        background-color: ${({ theme }) => theme.color.white};
+        display: flex;
+        flex-direction: column;
+        justify-content: space-around;
+        align-items: center;
+        margin-bottom: 20px;
+        margin-top: 70px;
+        color: white;
+
+        .wrapper {
+            width: 100%;
+            height: auto;
+            position: relative;
+            display: flex;
+            justify-content: space-evenly;
+            align-items: center;
+            margin-bottom: 7px;
+            padding-left: 30px;
+            padding-right: 30px;
+        }
+
+        .wrapper::before {
+            content: '';
+            width: 50%;
+            height: 2px;
+            background-color: ${({ theme }) => theme.color.lightgray};
+            position: absolute;
+            top: 50%;
+            left: 100;
+        }
+
+        .current_circle {
+            width: 160px;
+            height: 160px; 
+            position: relative;
+            border-radius: 50%;
+            background-color: ${({ theme }) => theme.color.blue};
+            text-align: center;
+        }
+
+        .left_circle {
+            width: 160px;
+            height: 160px;
+            position: relative;
+            border-radius: 50%;
+            background-color: ${({ theme }) => theme.color.lightgray};
+            text-align: center;
+        }
+    }
+`;
+
+const LoadingContainerDiv = styled(ContainerDiv)`
+    background-color: ${({ theme }) => theme.color.white};
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    align-items: center;
+    margin-bottom: 20px;
+    margin-top: 70px;
 `;
 
 const TextH1 = styled.h1`
@@ -117,23 +166,8 @@ const TextH2 = styled.h2`
         list-style: inside;
         margin-top: 20px;
     }
-    color: ${({ theme }) => theme.color.white};
-    background-color: ${props => props.theme.color.white};
-`;
-
-const LineDiv = styled.div`
-    @media ${({ theme }) => theme.device.mobile} {
-        .hr-sect::before,
-        .hr-sect::after {
-            content: '';
-            flex-grow: 1;
-            background-color: ${({ theme }) => theme.color.lightgray};
-            height: 1px;
-            font-size: 0px;
-            line-height: 0px;
-            margin: 0px;
-        }
-    }
+    margin-top: 40px;
+    font-weight:
     color: ${({ theme }) => theme.color.white};
     background-color: ${props => props.theme.color.white};
 `;

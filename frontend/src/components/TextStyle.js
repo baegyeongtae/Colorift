08be82fe174a893f23400684d09def8edd6e2ff3
misun <mcoding777@naver.com>
2022-02-19 21:36:24 +0900

    feat : 예시페이지 헤더 디자인 구현

--- frontend/src/components/TextStyle.js
+++ frontend/src/components/TextStyle.js
@@ -5,10 +5,12 @@ import styled from 'styled-components';
 export const BigTextP = styled.p`
     font-size: ${({ theme }) => theme.fontSizes.bigtext};
     font-weight: bold;
+    color: ${({ color }) => color};
 `;
 
 export const DescriptionP = styled.p`
     text-align: center;
+    color: ${({ color }) => color};
 
     @media ${({ theme }) => theme.device.laptop} {
         font-size: ${({ theme }) => theme.fontSizes.mediumtext};

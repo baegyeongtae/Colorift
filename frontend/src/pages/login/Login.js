c7d0a2e163e847a3e4d3c94bcc59638c18530f6f
misun <mcoding777@naver.com>
2022-02-24 18:16:32 +0900

    refactor : 로그인 페이지 div 태그를 form 변경

--- frontend/src/pages/login/Login.js
+++ frontend/src/pages/login/Login.js
@@ -12,12 +12,6 @@ import {
 import { setScrollDisabled } from '../../utils/data/setScrollDisabled';
 
 export function Login() {
-    // 로그인 더미 데이터
-    const dummyUser = {
-        email: 'admin@naver.com',
-        password: '123456789a!',
-    };
-
     const [findModal, setFindModal] = useState(false);
 
     function handleToggleModal() {
@@ -36,18 +30,19 @@ export function Login() {
                         <TitleP color="#3C64B1" className="column">
                             Login
                         </TitleP>
-                        <UserInputDiv text="Email" type="email" />
-                        <UserInputDiv text="Password" type="password" />
-                        <UserButton
-                            type="submit"
-                            width="80%"
-                            height="80%"
-                            className="login_button column"
-                            onClick={() => window.open('/', '_self')}
-                        >
-                            로그인
-                        </UserButton>
-
+                        <UserForm className="column">
+                            <UserInputDiv text="Email" type="email" />
+                            <UserInputDiv text="Password" type="password" />
+                            <UserButton
+                                type="submit"
+                                width="80%"
+                                height="80%"
+                                className="login_button column"
+                                onClick={() => window.open('/', '_self')}
+                            >
+                                로그인
+                            </UserButton>
+                        </UserForm>
                         <p>비밀번호를 잊어버리셨나요?</p>
                         <UserButton
                             type="button"
@@ -85,7 +80,7 @@ export const CenterContainerDiv = styled(ContainerDiv)`
 
 export const LoginDiv = styled.div`
     display: grid;
-    grid-template-rows: 1.5fr repeat(4, 1fr);
+    grid-template-rows: 1.5fr 2fr repeat(2, 1fr);
     grid-template-columns: repeat(2, 1fr);
     align-items: center;
     justify-items: center;
@@ -99,18 +94,11 @@ export const LoginDiv = styled.div`
         margin: 0 0 50px 10px;
     }
 
-    .login_button {
-        grid-column-start: 2;
-        grid-column-end: 3;
-        grid-row-start: 2;
-        grid-row-end: 4;
-    }
-
     @media screen and (max-width: 420px) {
         all: unset;
 
         display: grid;
-        grid-template-rows: repeat(3, 1fr) 2fr repeat(4, 0.5fr);
+        grid-template-rows: 1fr 4fr repeat(4, 0.5fr);
         align-items: center;
         justify-items: start;
 
@@ -128,21 +116,55 @@ export const LoginDiv = styled.div`
             grid-column-end: 2;
         }
 
+        .button {
+            width: 100%;
+            height: 100%;
+
+            margin-bottom: 20px;
+        }
+    }
+`;
+
+const UserForm = styled.form`
+    grid-column-start: 1;
+    grid-column-end: 3;
+
+    display: grid;
+    grid-template-rows: repeat(2, 1fr);
+    grid-template-columns: repeat(2, 1fr);
+    align-items: center;
+    justify-items: center;
+
+    .login_button {
+        grid-column-start: 2;
+        grid-column-end: 3;
+        grid-row-start: 1;
+        grid-row-end: 3;
+    }
+
+    @media screen and (max-width: 420px) {
+        all: unset;
+
+        display: grid;
+        grid-template-rows: repeat(2, 1fr) 2fr;
+        align-items: center;
+        justify-items: start;
+
+        font-size: 1.6rem;
+
+        .column {
+            grid-column-start: 1;
+            grid-column-end: 2;
+        }
+
         .login_button {
-            grid-row-start: 4;
+            grid-row-start: 3;
             grid-row-end: 5;
 
             width: 100%;
             height: 60%;
 
             margin-bottom: 30px;
-        }
-
-        .button {
-            width: 100%;
-            height: 100%;
-
-            margin-bottom: 20px;
         }
     }
 `;

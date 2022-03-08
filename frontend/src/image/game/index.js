c29e05677b9172422a0a623de1b92f475d567c59
misun <mcoding777@naver.com>
2022-03-08 18:18:59 +0900

    feat : 레이아웃 적용 및 봄 이미지 추가

---
+++ frontend/src/image/game/index.js
@@ -0,0 +1,65 @@
+import spring0 from './spring0.jpg';
+import spring1 from './spring1.jpg';
+import spring2 from './spring2.jpg';
+import spring3 from './spring3.jpg';
+import spring4 from './spring4.jpg';
+import spring5 from './spring5.png';
+import spring6 from './spring6.jpg';
+import spring7 from './spring7.png';
+import spring8 from './spring8.png';
+import spring9 from './spring9.jpg';
+
+const gameImages = [
+    {
+        img: spring0,
+        name: '슈퍼주니어 규현',
+        season: 'spring',
+    },
+    {
+        img: spring1,
+        name: '윤세아',
+        season: 'spring',
+    },
+    {
+        img: spring2,
+        name: '김선호',
+        season: 'spring',
+    },
+    {
+        img: spring3,
+        name: '수지',
+        season: 'spring',
+    },
+    {
+        img: spring4,
+        name: '아이유',
+        season: 'spring',
+    },
+    {
+        img: spring5,
+        name: '육성재',
+        season: 'spring',
+    },
+    {
+        img: spring6,
+        name: '정소민',
+        season: 'spring',
+    },
+    {
+        img: spring7,
+        name: '천우희',
+        season: 'spring',
+    },
+    {
+        img: spring8,
+        name: '한지민',
+        season: 'spring',
+    },
+    {
+        img: spring9,
+        name: '한효주',
+        season: 'spring',
+    },
+];
+
+export { gameImages };
--- frontend/src/pages/game/GameStart.js
+++ frontend/src/pages/game/GameStart.js
@@ -5,11 +5,62 @@ export function GameStart() {
     return (
         <Article>
             <NavBackgroundDiv />
-            <ContainerDiv>
-                <p>안녕하세요</p>
-            </ContainerDiv>
+            <GameContainerDiv>
+                <p>
+                    나는야 퍼스널 컬러 전문가!
+                    <br />
+                    연예인의 퍼스널 컬러를 맞춰보세요 😀
+                </p>
+                <ImageDiv>연예인 사진</ImageDiv>
+                <ButtonsDiv>
+                    <ColorDiv>봄 웜톤</ColorDiv>
+                    <ColorDiv>여름 쿨톤</ColorDiv>
+                    <ColorDiv>가을 웜톤</ColorDiv>
+                    <ColorDiv>겨울 쿨톤</ColorDiv>
+                </ButtonsDiv>
+            </GameContainerDiv>
         </Article>
     );
 }
 
 // styled-components
+
+const GameContainerDiv = styled(ContainerDiv)`
+    ${({ theme }) => theme.flexStyled.flexColumn};
+    ${({ theme }) => theme.flexStyled.flexCenter};
+
+    p {
+        font-size: ${({ theme }) => theme.fontSizes.bigtext};
+        font-weight: bold;
+        text-align: center;
+    }
+`;
+
+const ImageDiv = styled.div`
+    width: 400px;
+    height: 500px;
+
+    margin: 5vh 0;
+
+    background-color: gray;
+`;
+
+const ButtonsDiv = styled.div`
+    width: 100%;
+
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    grid-column-gap: 10px;
+`;
+
+const ColorDiv = styled.div`
+    width: 200px;
+    height: 50px;
+
+    border-radius: 100px;
+
+    text-align: center;
+    line-height: 50px;
+
+    background-color: ${({ theme }) => theme.color.lightgray};
+`;

1ad6eb325af9123800024c5cd83045af44b41772
misun <mcoding777@naver.com>
2022-03-09 17:09:35 +0900

    feat : 미니게임 접근 시 스크롤바 최상단 이동 추가

--- frontend/src/pages/game/Game.js
+++ frontend/src/pages/game/Game.js
@@ -1,6 +1,7 @@
 import { useState, useMemo } from 'react';
 import { GameStart, GameEnd } from '.';
 import gameImages from '../../image/game';
+import { useScrollToTop } from '../../utils/hooks/useScrollToTop';
 
 export function Game() {
     // 재도전 state
@@ -29,6 +30,9 @@ export function Game() {
         setRetry(current => current + 1);
         setGamePage(0);
     };
+
+    // 한번 게임이 진행될 때마다 스크롤 위로 올리기 (모바일용)
+    useScrollToTop(retry);
 
     // 페이지에 따라 컴포넌트 렌더링할 함수
     function renderHTML() {

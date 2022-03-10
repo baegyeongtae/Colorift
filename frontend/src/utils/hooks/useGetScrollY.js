708d1c20c69d5f23517fa4965b58400cdb7c1f96
misun <mcoding777@naver.com>
2022-03-10 16:38:27 +0900

    refactor : 메인페이지 scrollY값 함수 최적화

--- frontend/src/utils/hooks/useGetScrollY.js
+++ frontend/src/utils/hooks/useGetScrollY.js
@@ -1,15 +1,15 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useMemo } from 'react';
 import { useLocation } from 'react-router-dom';
 
 export function useGetScrollY() {
     // 현재 url 받아오기
     const location = useLocation();
-    const { pathname } = location;
+    const { pathname } = useMemo(() => location, [location.pathname]);
 
     const [scrollY, setScrollY] = useState(0);
 
     const listener = () => {
-        setScrollY(window.pageYOffset); // window.pageYOffset은 window.scrollY와 동일함 (오래된 브라우저 호환)
+        if (window.pageYOffset <= 1) setScrollY(window.pageYOffset); // window.pageYOffset은 window.scrollY와 동일함 (오래된 브라우저 호환)
     };
 
     useEffect(() => {

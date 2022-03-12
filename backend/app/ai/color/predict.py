827bf5cc7def83c315be746d5ee78f681000da62
[3기-2반] 심소민 <shimsomin20@gmail.com>
2022-03-12 00:11:56 +0000

    feat: add comment

--- backend/app/ai/color/predict.py
+++ backend/app/ai/color/predict.py
@@ -10,17 +10,21 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
 
 def predict_color(file):
+    """
+    Personal Color Diagnosis
+    """
     nparr = np.fromstring(file.read(), np.uint8)
     img = cv.imdecode(nparr, cv.IMREAD_COLOR)
     img = cv.resize(img, dsize=(160, 160), interpolation=cv.INTER_AREA)
     img = np.expand_dims(img, axis=0)
-    
-    #model_path = '/app/ai/color/models/mobilenet_v2.h5'
-    #mobilenet_v2 = keras.models.load_model(model_path)
-    probability_model = tf.keras.Sequential([LoadConfig.mobilenet_v2, tf.keras.layers.Softmax()])
 
+    probability_model = tf.keras.Sequential([LoadConfig.mobilenet_v2, tf.keras.layers.Softmax()])
     pred_class = probability_model.predict(img)
+
     result = pred_class[0]
     result = list(map(lambda x: round(x * 100), result))
+
+    # result = ['autumn', 'spring', 'summer', 'winter'] 순서로 저장됨.
     data = {'spring_rate': result[1],'summer_rate':result[2],'autumn_rate':result[0],'winter_rate':result[3]}
+
     return data
